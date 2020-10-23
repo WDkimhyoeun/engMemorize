0a2941204661652aa02662ae214fa586c6f57758
WDkimhyoeun <66587298+WDkimhyoeun@users.noreply.github.com>
2020-10-23 17:05:02 +0900

    JSON 데이터 불러오기

--- js/check.js
+++ js/check.js
@@ -2,16 +2,15 @@
 function loadItems() {
     return fetch('../data/data.json')
     .then(response => response.json())
-    .then(json => json.items);
 }
+
 
 // main
 // promise가 성공적으로 값을 전달해주면, 전달받은 items를 이용해서 displayItems를 보여주고
 // EventListeners를 등록해서 적절한 item을 filterling해주는 것까지!
+
 loadItems()
-.then(items => {
-    console.log(items);
+.then(items => console.log(JSON.stringify(items)))
     //displayItems(items);
     // setEventListeners(items)
-})
 .catch(console.log);
